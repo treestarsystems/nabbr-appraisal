80e9691b0d518dafc48c471e30de7e720fba4678
treestarsystems <51288647+treestarsystems@users.noreply.github.com>
2024-11-22 00:30:08 -0500

    tweak route names

--- backend/api/routes.go
+++ backend/api/routes.go
@@ -7,8 +7,8 @@ import (
 func RoutesAppraisal(router *gin.Engine) {
 	appraisal := router.Group("/api/v1/appraisal")
 	{
-		appraisal.GET("/template/chart", GetAppraisalChartTemplateJSON)
 		appraisal.GET("/chart", GetAppraisalChartAllJSON)
+		appraisal.GET("/chart/template", GetAppraisalChartTemplateJSON)
 		appraisal.GET("/chart/:appraisalId", GetAppraisalChartByIdJSON)
 		appraisal.POST("/chart", PostPutAppraisalChartJSON)
 		appraisal.PUT("/chart/:appraisalId", PostPutAppraisalChartJSON)
