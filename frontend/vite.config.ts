5c03a88ed015e50a0bbaf1e772250dce615a27cb
Tree Star Systems <info@treestarsystems.com>
2024-12-27 12:23:46 -0500

    fixing build issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -16,6 +16,16 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: '../dist/frontend/',
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          },
+        },
+      },
     },
   };
 });
